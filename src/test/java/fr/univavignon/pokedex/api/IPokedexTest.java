0d830bceddb16d958f348fc7bbab5acde5d5339c
AnthonyLegendre <anthony.legendre@alumni.univ-avignon.fr>
2017-03-20 11:32:10 +0100

    Add test

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -2,28 +2,33 @@ package fr.univavignon.pokedex.api;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.*;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 public class IPokedexTest {
-	
+
+	@Mock private IPokedex pokedexMock;
+	@Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
+
 	@Test
 	public void testSum() {
 		final int sum = 2 + 2;
 		assertEquals(4, sum);
 	}
 	
-	/*
 	@Test
 	public void testSize() {
 		IPokedex pokedex = getTestPokedex();
 		
 		assertEquals(pokedex.size(), 151);
-	}*/
+	}
 	
 	
-	/*public IPokedex getTestPokedex()
+	public IPokedex getTestPokedex()
 	{
-		IPokedex pokedex;
-		return pokedex;
-	}*/
+		return pokedexMock;
+	}
 }
