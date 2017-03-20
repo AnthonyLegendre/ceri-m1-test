f64c8f7a4cd5a0635762263e0e3ccfed05ba0619
AnthonyLegendre <anthony.legendre@alumni.univ-avignon.fr>
2017-03-20 11:00:53 +0100

    Add test

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -0,0 +1,13 @@
+package fr.univavignon.pokedex.api;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IPokedexTest {
+	@Test
+	public void testSum() {
+		final int sum = 2 + 2;
+		assertEquals(4, sum);
+	}
+}
