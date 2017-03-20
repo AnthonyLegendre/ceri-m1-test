927de16bb61c3f87ebc0a63f5927c8344140ab1d
AnthonyLegendre <anthony.legendre@alumni.univ-avignon.fr>
2017-03-20 11:20:12 +0100

    test

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -5,9 +5,25 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class IPokedexTest {
+	
 	@Test
 	public void testSum() {
 		final int sum = 2 + 2;
 		assertEquals(4, sum);
 	}
+	
+	/*
+	@Test
+	public void testSize() {
+		IPokedex pokedex = getTestPokedex();
+		
+		assertEquals(pokedex.size(), 151);
+	}*/
+	
+	
+	/*public IPokedex getTestPokedex()
+	{
+		IPokedex pokedex;
+		return pokedex;
+	}*/
 }
