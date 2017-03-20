06f69f69bfc5c8ea0738569a652a4840ac461072
AnthonyLegendre <anthony.legendre@alumni.univ-avignon.fr>
2017-03-20 13:37:06 +0100

    Add test PokemonMetadataProvider

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -1,7 +1,36 @@
 package fr.univavignon.pokedex.api;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.mockito.*;
+
 public class IPokemonMetadataProviderTest {
 	
+	@Test
+	public void testPkmMetadata()
+	{
+		PokemonMetadata pkm = getPkmMetadata(0);
+		
+		assertEquals(0, pkm.getIndex());
+		assertEquals("Bulbizarre", pkm.getName());
+		assertEquals(126, pkm.getAttack());
+		assertEquals(126, pkm.getDefense());
+		assertEquals(90, pkm.getStamina());
+	}
+	
+	@Test
+	public void testException()
+	{
+		//Todo
+	}
+	
+	public PokemonMetadata getPkmMetadata(int index)
+	{
+		PokemonMetadata bulbi = new PokemonMetadata(0, "Bulbizarre", 126, 126, 90);
+		
+		return bulbi;
+	}
 	
 	
 	/* Méthode(s) de la classe à tester 
--- src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
@@ -1,12 +1,31 @@
 package fr.univavignon.pokedex.api;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.mockito.*;
+
 public class IPokemonTrainerFactoryTest {
 
+	/*private PokemonTrainer trainer;
 	
+	@Test
+	public void testTrainerFactory()
+	{
+		IPokemonTrainerFactory factory = getTrainerFactory();
+		PokemonTrainer trainer = factory.createTrainer("Sacha", Team.VALOR, IPokedexTest.getTestPokedex());
+		
+	}
+	
+	
+	public IPokemonTrainerFactory getTrainerFactory()
+	{
+		IPokemonTrainerFactory factory;
+		
+		return factory;
+	}
+	*/
 	/* Méthode(s) de la classe à tester 
-	 * PokemonTrainer(final String name, final Team team, final IPokedex pokedex)
-	 *  String getName() 
-	 *  Team getTeam() 
-	 *  IPokedex getPokedex()
+	 * PokemonTrainer createTrainer(String name, Team team, IPokedexFactory pokedexFactory);
 	 */
 }
