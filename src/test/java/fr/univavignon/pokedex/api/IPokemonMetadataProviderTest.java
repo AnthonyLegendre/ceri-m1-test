c61d3bc4aa8990763da552ecf392191da60a4e5f
AnthonyLegendre <anthony.legendre@alumni.univ-avignon.fr>
2017-04-19 16:42:07 +0200

    test ? Please fonctionne

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -8,40 +8,44 @@ import org.mockito.*;
 
 public final class IPokemonMetadataProviderTest {
 	
-	@Mock private IPokemonMetadataProvider pkmProvider;
+	
 	private PokemonMetadata bulbi = new PokemonMetadata(0, "Bulbizarre", 126, 126, 90);
 	private PokemonMetadata aquali = new PokemonMetadata(133, "Aquali", 186, 168, 260);
 	
-	
+	public IPokemonMetadataProvider getProvider()
+	{
+		IPokemonMetadataProvider pkmProvider = Mockito.mock(IPokemonMetadataProvider.class);
+		return pkmProvider;
+	}
 	
 	@Test
 	public void testPkmMetadata() throws PokedexException
 	{
-		assertEquals(bulbi, pkmProvider.getPokemonMetadata(0));
+		assertEquals(bulbi, this.getProvider().getPokemonMetadata(0));
 		
 	}
 	
 	@Test(expected=PokedexException.class)
 	public void testExceptionNeg() throws PokedexException
 	{
-		pkmProvider.getPokemonMetadata(-1);
+		this.getProvider().getPokemonMetadata(-1);
 	}
 	
 	@Test(expected=PokedexException.class)
 	public void testExceptionSup() throws PokedexException
 	{
-		pkmProvider.getPokemonMetadata(151);
+		this.getProvider().getPokemonMetadata(151);
 	}
 	
 	@Before 
 	public void setUp() throws PokedexException
 	{
 		MockitoAnnotations.initMocks(this);
-		Mockito.when(pkmProvider.getPokemonMetadata(0)).thenReturn(bulbi);
-		Mockito.when(pkmProvider.getPokemonMetadata(133)).thenReturn(aquali);
+		Mockito.when(this.getProvider().getPokemonMetadata(0)).thenReturn(bulbi);
+		Mockito.when(this.getProvider().getPokemonMetadata(133)).thenReturn(aquali);
 		
-		Mockito.when(pkmProvider.getPokemonMetadata(-1)).thenThrow(new PokedexException("Index inexistant"));
-		Mockito.when(pkmProvider.getPokemonMetadata(151)).thenThrow(new PokedexException("Index inexistant"));
+		Mockito.when(this.getProvider().getPokemonMetadata(-1)).thenThrow(new PokedexException("Index inexistant"));
+		Mockito.when(this.getProvider().getPokemonMetadata(151)).thenThrow(new PokedexException("Index inexistant"));
 	}
 	
 	/* Méthode(s) de la classe à tester 
