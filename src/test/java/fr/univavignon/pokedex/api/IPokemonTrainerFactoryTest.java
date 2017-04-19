7cb034508aa666d8a9b6b06acc7afc30d3bef3b3
AnthonyLegendre <anthony.legendre@alumni.univ-avignon.fr>
2017-04-19 12:07:24 +0200

    test

--- src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
@@ -50,7 +50,7 @@ public final class IPokemonTrainerFactoryTest {
 		Mockito.when(pokedexFactory.createPokedex(provider, pkmFactory)).thenReturn(pokedex);
 		
 		//Mock du trainer
-		Mockito.when(trainerFactory.createTrainer("Sacha", Team.VALOR, pokedexFactory)).thenReturn(sacha);
+		//Mockito.when(trainerFactory.createTrainer("Sacha", Team.VALOR, pokedexFactory)).thenReturn(sacha);
 		Mockito.when(trainerFactory.createTrainer(null, null, null)).thenReturn(this.sacha);
 				
 	}
